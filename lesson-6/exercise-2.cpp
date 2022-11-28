321479991dc72962e8f0c18d71d12347dd95614f
JanekAgent <jasiusko1@gmail.com>
2022-11-28 16:21:14 +0100

    catch in main not in class

--- lesson-6/exercise-2.cpp
+++ lesson-6/exercise-2.cpp
@@ -15,26 +15,43 @@ class Logarithm : public Number {
     public:
     double doubleValue() { return log(this->argument) / log(this->base);}
     Logarithm(double inputBase, double inputArgument) {
-    try{
-    if (inputBase<=0){throw(1);};
-    if (inputArgument<0){throw(2);};
-    }
-    catch (int errorNumber){
-        if (errorNumber=1){
-            cout<<"Wrong value of base";
-            return;
-        }
-        if (errorNumber){
-            cout<<"Wrong value of argument";
-            return;
-        }
-    }
+    
+    if (inputBase<=0){throw(1);}
+    if (inputBase==1){throw(1);}
+    if (inputArgument<0){throw(2);}
+    
+    
+    //cout<<"elding\n";
     this->base = inputBase;
     this->argument = inputArgument;
 
 }
 };
 int main(){
-    Logarithm lg(10,10);
-    cout<<lg.doubleValue();
+    Logarithm lg(10,1000000);
+    int correct=1;
+    
+    
+
+    try{
+    Logarithm lg(10,1230);}
+
+    catch (int errorNumber){
+        correct=0;
+        if (errorNumber==1){
+            cout<<"Wrong value of base\n";
+
+            //cout<<"Inputing standard value\n";
+                        
+            //return;
+        }
+        if (errorNumber==2){
+            cout<<"Wrong value of argument\n";
+            //cout<<"Inputing standard value\n";
+            //return;
+
+        }
+    }
+    if (correct==1){
+    cout<<lg.doubleValue()<<"\n";}
 }
