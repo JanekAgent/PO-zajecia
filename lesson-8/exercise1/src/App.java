7d54cf5430ff8807dfd63ff10b96a22d4d9fe966
JanekAgent <jasiusko1@gmail.com>
2022-12-12 16:21:19 +0100

    help

--- lesson-8/exercise1/src/App.java
+++ lesson-8/exercise1/src/App.java
@@ -1,5 +1,10 @@
-public class App {
-    public static void main(String[] args) throws Exception {
+public class App{
+
+    public static void mian(String[] args){
+        Exchange exchange= new Exchange.Fake();
+        Cash dollar = new Cash(exchange,500);
+        Cash euro = dollar.exchangedCash("EUR");
+        assert "617.25".equals(euro.toString());
         System.out.println("Hello, World!");
     }
 }
---
+++ lesson-8/exercise1/src/Weather.java
@@ -0,0 +1,6 @@
+public class Weather{
+    private final float ammount;
+    public Weather(float am){
+        this.ammount=am;
+    }
+}
